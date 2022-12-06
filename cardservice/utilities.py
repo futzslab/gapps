d6a20195e6aa80556b5bdf2ce278c7e36f35c188
Serge Koudoro <skab12@gmail.com>
2022-12-06 09:39:22 -0500

    add decode_user and decode_email utilities func

--- cardservice/utilities.py
+++ cardservice/utilities.py
@@ -1,7 +1,22 @@
 
+from google.auth.transport import requests
+from google.oauth2.id_token import verify_oauth2_token
+
 
 def delete_none(_dict):
-    """Delete None values recursively from all of the dictionaries"""
+    """Delete None values recursively from all of the dictionaries
+
+    Parameters
+    ----------
+    _dict : dict
+        dictionnaries containing None values
+
+    Returns
+    -------
+    _dict : dict
+        dictionnaries without any None values
+
+    """
     for key, value in list(_dict.items()):
         if isinstance(value, dict):
             delete_none(value)
@@ -13,3 +28,44 @@ def delete_none(_dict):
                     delete_none(v_i)
 
     return _dict
+
+
+def decode_user(token: str):
+    """Decode Google User ID Token.
+
+    Parameters
+    ----------
+    token : str
+        An end user ID token. The user’s email address is encoded in
+        the end user ID token, formatted as a JSON web token
+
+    Returns
+    -------
+    dict
+        user id info
+
+    """
+    request = requests.Request()
+    id_info = verify_oauth2_token(token, request)
+    # TODO: Handle the valueerror below
+    # ValueError: Token expired, 1659048959 < 1659050005
+    return id_info
+
+
+def decode_email(token: str):
+    """Decode Google User ID Token.
+
+    Parameters
+    ----------
+    token : str
+        An end user ID token. The user’s email address is encoded in
+        the end user ID token, formatted as a JSON web token
+
+    Returns
+    -------
+    dict
+        user id info
+
+    """
+    user = decode_user(token)
+    return user.get("email", "")
